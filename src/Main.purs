8a709a9a3740c921b902af00e2481924390f61d7
atiredturtle <guy@segev.com>
2020-08-04 00:35:16 +1000

    adds basic effect typing and plays around'

--- src/Main.purs
+++ src/Main.purs
@@ -2,16 +2,21 @@ module Main where
 
 import Prelude
 import Effect (Effect)
-import Control.Monad.Eff.Random (random)
+import Effect.Random (random)
 import Effect.Console (log, logShow)
 
 
 mySum :: Int -> Int -> Int
 mySum x y = x + y
 
+getRandomNum :: Effect String
+getRandomNum = do
+    r <- random
+    pure (show r)
+
 main :: Effect Unit
 main = do
-    r <- randomInt
-    log "Random Int" ++ r
+    r <- getRandomNum
+    log ("Random Int: " <> show r)
     logShow (mySum 3 2)
     log "Hello sailor!"
