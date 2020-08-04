74bac0759f4c3bd67a1c07cc753712fe8fdc7b38
atiredturtle <guy@segev.com>
2020-08-04 18:30:10 +1000

    draws rectangles

--- src/Main.purs
+++ src/Main.purs
@@ -1,22 +1,38 @@
 module Main where
 
 import Prelude
+import Data.Maybe
+import Partial.Unsafe (unsafePartial)
 import Effect (Effect)
-import Effect.Random (random)
 import Effect.Console (log, logShow)
+import Graphics.Canvas(rect, Rectangle, Context2D, getContext2D, getCanvasElementById, setFillStyle, fillPath, setCanvasWidth, setCanvasHeight)
 
+type CanvasSettings = {width:: Number, height:: Number, color:: String}
+canvasSettings :: CanvasSettings
+canvasSettings = {width: 600.0, height: 600.0, color: "black"}
 
-mySum :: Int -> Int -> Int
-mySum x y = x + y
+fillRectangle ::Context2D -> String -> Rectangle -> Effect Unit
+fillRectangle ctx color r = do 
+    setFillStyle ctx color
+    fillPath ctx $ rect ctx r
 
-getRandomNum :: Effect String
-getRandomNum = do
-    r <- random
-    pure (show r)
+clearCanvas :: Context2D -> CanvasSettings -> Effect Unit
+clearCanvas ctx settings = let 
+    r = { x: 0.0
+        , y: 0.0
+        , width: canvasSettings.width
+        , height: canvasSettings.height
+        }
+    in do 
+    fillRectangle ctx canvasSettings.color r
+
 
 main :: Effect Unit
-main = do
-    r <- getRandomNum
-    log ("Random Int: " <> show r)
-    logShow (mySum 3 2)
-    log "Hello sailor!"
+main = void $ unsafePartial do
+    log "Game started!"
+    Just canvas <- getCanvasElementById "myCanvas"
+    -- Set canvas dimensions
+    setCanvasWidth canvas canvasSettings.width 
+    setCanvasHeight canvas canvasSettings.height
+    ctx <- getContext2D canvas
+    clearCanvas ctx canvasSettings
